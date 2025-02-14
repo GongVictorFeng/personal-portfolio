a26b63ad6e0fb434e398cd49a38abe436870d613
victor <gfeng005@uottawa.ca>
2025-02-13 21:20:43 -0500

    added nav to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 @Component({
   selector: 'app-root',
   imports: [
     MatSlideToggleModule,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
