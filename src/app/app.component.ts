2e8078603f9bc676c3ef7f27b9ad2293e97e51f8
victor <gfeng005@uottawa.ca>
2025-02-14 18:21:45 -0500

    Created components and set up routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   imports: [
     MatSlideToggleModule,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    RouterOutlet,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   imports: [
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
