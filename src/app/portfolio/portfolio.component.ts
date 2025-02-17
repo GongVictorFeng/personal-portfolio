4128b597eae45fa15c0485e054186ed65f79a6a4
victor <gfeng005@uottawa.ca>
2025-02-16 19:41:56 -0500

    created the preliminary portfolio component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ProjectCardComponent } from './project-card/project-card.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 
 @Component({
   selector: 'app-portfolio',
   imports: [
-    ProjectCardComponent
+    ProjectCardComponent,
+    MatGridListModule
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+
+  project: Project = {
+    id: 0,
+    name: "Sample Angular Project",
+    summary: "Test description",
+    description: "",
+    projectLink: "",
+    url: "",
+    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    pictures: []
+  }
   
     constructor(private titleService: Title) {
       this.titleService.setTitle('Gong(Victor) Feng - Portfolio')
