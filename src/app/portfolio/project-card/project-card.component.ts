64734c14dc8316800a747c4083dc7cebd3d07dd8
victor <gfeng005@uottawa.ca>
2025-02-17 16:15:09 -0500

    added event listener to view more button, so that a project dialog will open while clicking the button

--- src/app/portfolio/project-card/project-card.component.ts
+++ src/app/portfolio/project-card/project-card.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatChipsModule} from '@angular/material/chips';
 import { Project } from '../../models/Project';
 import { CommonModule } from '@angular/common';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { ProjectDialogComponent } from '../../project-dialog/project-dialog.component';
 
 @Component({
   selector: 'app-project-card',
@@ -11,11 +13,24 @@ import { CommonModule } from '@angular/common';
     MatCardModule,
     MatButtonModule,
     MatChipsModule,
-    CommonModule
+    CommonModule,
+    MatDialogModule
   ],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
 export class ProjectCardComponent {
+
     @Input() project: Project = {} as Project;
+
+    constructor(private dialog: MatDialog) {}
+
+    openDialog() {
+        this.dialog.open(ProjectDialogComponent, {
+          data: this.project,
+          width: 'auto',
+          maxHeight: '90vh',
+          maxWidth: '50vw'
+        })
+      }
 }
