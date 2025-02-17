24f83a2aee2364a7e17cb78eae447e22d708150c
victor <gfeng005@uottawa.ca>
2025-02-16 23:50:32 -0500

    injected the project service to retrieve projects, then used ngfor to projector all projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,33 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ProjectCardComponent } from './project-card/project-card.component';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { Project } from '../_models/Project';
-import { Tag } from '../_models/Tag';
+import { Project } from '../models/Project';
+import { CommonModule } from '@angular/common';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-portfolio',
   imports: [
     ProjectCardComponent,
-    MatGridListModule
+    MatGridListModule,
+    CommonModule
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
 
-  project: Project = {
-    id: 0,
-    name: "Sample Angular Project",
-    summary: "Test description",
-    description: "",
-    projectLink: "",
-    url: "",
-    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
-    pictures: []
-  }
+  projects = {} as Project[];
   
-    constructor(private titleService: Title) {
+  constructor(private titleService: Title, private projectService: ProjectService) {
       this.titleService.setTitle('Gong(Victor) Feng - Portfolio')
     }
+  ngOnInit(): void {
+    this.projects = this.projectService.getProjects();
+  }
 }
