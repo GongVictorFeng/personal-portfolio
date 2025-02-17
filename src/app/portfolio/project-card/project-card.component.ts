3d6cf4f297c118872b3327bfa980d039e7d9c606
victor <gfeng005@uottawa.ca>
2025-02-16 19:40:55 -0500

    completed the creation of project card component

--- src/app/portfolio/project-card/project-card.component.ts
+++ src/app/portfolio/project-card/project-card.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import {MatChipsModule} from '@angular/material/chips';
+import { Project } from '../../_models/Project';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
   imports: [
     MatCardModule,
     MatButtonModule,
-    MatChipsModule
+    MatChipsModule,
+    CommonModule
   ],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
 export class ProjectCardComponent {
-
+    @Input() project: Project = {} as Project;
 }
