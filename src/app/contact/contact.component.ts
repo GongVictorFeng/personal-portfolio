d085085de22cc7e864aae58cd989b4b465c7dfb1
victor <gfeng005@uottawa.ca>
2025-02-18 17:50:55 -0500

    completed the contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { Title } from '@angular/platform-browser';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatListModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  email: string = "gn927843@dal.ca"
     constructor(private titleService: Title) {
-      this.titleService.setTitle('Gong(Victor) Feng - Contact')
+
+      this.titleService.setTitle('Gong(Victor) Feng - Contact');
     }
 }
