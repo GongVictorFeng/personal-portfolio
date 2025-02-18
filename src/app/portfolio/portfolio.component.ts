7898a51069df4a688afe053229e5e3c6d3e7880c
victor <gfeng005@uottawa.ca>
2025-02-17 22:46:35 -0500

    added filter button and filter card to check tags

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -5,13 +5,22 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { Project } from '../models/Project';
 import { CommonModule } from '@angular/common';
 import { ProjectService } from '../services/project.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { Tag } from '../models/Tag';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-portfolio',
   imports: [
     ProjectCardComponent,
     MatGridListModule,
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
@@ -19,11 +28,42 @@ import { ProjectService } from '../services/project.service';
 export class PortfolioComponent implements OnInit {
 
   projects = {} as Project[];
+  isCollapsed: boolean = true;
+  // Define filters object
+  filters: { [key: string]: boolean } = {};
+  languages = [
+    { key: 'java', label: 'Java' },
+    { key: 'typescript', label: 'TypeScript' },
+    { key: 'csharp', label: 'C#' },
+    { key: 'python', label: 'Python' },
+    { key: 'javascript', label: 'JavaScript' }
+  ];
+
+  // Define frameworks dynamically
+  frameworks = [
+    { key: 'angular', label: 'Angular' },
+    { key: 'react', label: 'React' },
+    { key: 'spring', label: 'Spring Boot' },
+    { key: 'dotnet', label: '.NET' }
+  ];
   
   constructor(private titleService: Title, private projectService: ProjectService) {
-      this.titleService.setTitle('Gong(Victor) Feng - Portfolio')
+      this.titleService.setTitle('Gong(Victor) Feng - Portfolio');
+
+      [...this.languages, ...this.frameworks].forEach(item => {
+        this.filters[item.key] = false;
+      });
     }
   ngOnInit(): void {
     this.projects = this.projectService.getProjects();
+  }
+
+  onFilter() {
+    const filterTag: Tag[] = [];
+    
+    if (this.filters['typescript']) {
+      filterTag.push(Tag.TYPESCRIPT)
+    }
+    this.projects = this.projectService.getProjectsByFilter(filterTag)
   }
 }
