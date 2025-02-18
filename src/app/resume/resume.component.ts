049dd20ae35d30ed7917c5d80d5b3adc02c2b955
victor <gfeng005@uottawa.ca>
2025-02-18 11:07:14 -0500

    added feature to download resume by clicking a btn in resume component

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resume',
-  imports: [],
+  imports: [
+    MatButtonModule,
+  ],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
-    constructor(private titleService: Title) {
+    constructor(private titleService: Title, private renderer: Renderer2) {
       this.titleService.setTitle('Gong(Victor) Feng - Resume')
+    }
+
+    onDownload() {
+      const link = this.renderer.createElement('a');
+      link.setAttribute('target', '_blank');
+      link.setAttribute('href', '../../assets/Gong_resume_2025.pdf');
+      link.setAttribute('download', 'Gong_resume_2025.pdf');
+      link.click();
+      link.remove();
     }
 }
