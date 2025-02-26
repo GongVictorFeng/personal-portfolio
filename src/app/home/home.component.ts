1ec66fb77de1d4302275215d05127fa8851a6b96
victor <gfeng005@uottawa.ca>
2025-02-26 11:31:23 -0500

    added responsive screen for home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import { Project } from '../models/Project';
 import { ProjectService } from '../services/project.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -18,10 +19,36 @@ export class HomeComponent implements OnInit {
 
   featureProject = {} as Project;
 
-    constructor(private titleService: Title, private projectService: ProjectService) {
-      this.titleService.setTitle('Gong(Victor) Feng - Home')
-    }
+  cols = 2;
+  rowHeight = "65vh";
+
+  constructor(private titleService: Title, private projectService: ProjectService, private responsive: BreakpointObserver) {
+    this.titleService.setTitle('Gong(Victor) Feng - Home')
+  }
   ngOnInit(): void {
     this.featureProject = this.projectService.getProjectById(0);
+
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.TabletLandscape,
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape
+    ]).subscribe(
+      result => {
+        this.cols = 2;
+        this.rowHeight = "65vh";
+
+        const breakpoints = result.breakpoints;
+
+        if (breakpoints[Breakpoints.TabletPortrait] || breakpoints[Breakpoints.TabletLandscape]) {
+            this.cols = 1;
+            this.rowHeight = "65vh";
+        }
+        else if (breakpoints[Breakpoints.HandsetPortrait] || breakpoints[Breakpoints.HandsetLandscape]) {
+            this.cols = 1;
+            this.rowHeight = "100vh";
+        }
+      }
+    )
   }
 }
