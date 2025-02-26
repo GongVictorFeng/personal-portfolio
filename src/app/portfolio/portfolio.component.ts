1097c0661a0fa98db27959e8bfbd22968f889dc3
victor <gfeng005@uottawa.ca>
2025-02-26 11:00:51 -0500

    added responsive screen to the portfolio component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ProjectCardComponent } from './project-card/project-card.component';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { Project } from '../models/Project';
 import { CommonModule } from '@angular/common';
 import { ProjectService } from '../services/project.service';
@@ -10,6 +10,7 @@ import { MatCardModule } from '@angular/material/card';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { Tag } from '../models/Tag';
 import { FormsModule } from '@angular/forms';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-portfolio',
@@ -31,6 +32,9 @@ export class PortfolioComponent implements OnInit {
   isCollapsed: boolean = true;
   isFiltered: boolean = false;
 
+  cols = 3;
+  rowHeight ="200px"
+
   // Define filters object
   filters: { [key: string]: boolean } = {};
   languages = [
@@ -49,7 +53,7 @@ export class PortfolioComponent implements OnInit {
     Tag.NODEJS
   ];
   
-  constructor(private titleService: Title, private projectService: ProjectService) {
+  constructor(private titleService: Title, private projectService: ProjectService, private responsive: BreakpointObserver) {
       this.titleService.setTitle('Gong(Victor) Feng - Portfolio');
 
       [...this.languages, ...this.frameworks].forEach(item => {
@@ -58,6 +62,33 @@ export class PortfolioComponent implements OnInit {
     }
   ngOnInit(): void {
     this.projects = this.projectService.getProjects();
+
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.TabletLandscape,
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape
+    ]).subscribe(
+      result => {
+        this.cols = 3;
+        this.rowHeight = "200px";
+
+        const breakpoints = result.breakpoints;
+
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+            this.cols = 2;
+        }
+        else if (breakpoints[Breakpoints.HandsetPortrait]) {
+            this.cols = 1;
+        }
+        else if (breakpoints[Breakpoints.HandsetLandscape]) {
+            this.cols = 1;
+        }
+        else if (breakpoints[Breakpoints.TabletLandscape]) {
+            this.cols = 2;
+        }
+      }
+    );
   }
 
   onFilter() {
