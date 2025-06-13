15aa8a91ee6a63936d62c3995cddabb9fad5af64
victor <gfeng005@uottawa.ca>
2025-06-12 23:52:37 -0400

    updated the resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,28 +1,25 @@
 import { Component, Renderer2 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Title } from '@angular/platform-browser';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-resume',
-  imports: [
-    MatButtonModule,
-    CdkAccordionModule
-  ],
+  imports: [MatButtonModule, CdkAccordionModule],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.scss'
+  styleUrl: './resume.component.scss',
 })
 export class ResumeComponent {
-    constructor(private titleService: Title, private renderer: Renderer2) {
-      this.titleService.setTitle('Gong(Victor) Feng - Resume')
-    }
+  constructor(private titleService: Title, private renderer: Renderer2) {
+    this.titleService.setTitle('Gong(Victor) Feng - Resume');
+  }
 
-    onDownload() {
-      const link = this.renderer.createElement('a');
-      link.setAttribute('target', '_blank');
-      link.setAttribute('href', '../../assets/Gong_resume_2025.pdf');
-      link.setAttribute('download', 'Gong_resume_2025.pdf');
-      link.click();
-      link.remove();
-    }
+  onDownload() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/Resume.pdf');
+    link.setAttribute('download', 'Gong_resume_2025.pdf');
+    link.click();
+    link.remove();
+  }
 }
